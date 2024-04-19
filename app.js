adfca4b1c5f5dcb1462fc245372798b518f8ffc1
jasmeet <jasmeet181994@gmail.com>
2024-04-19 16:16:01 +0530

    app using express

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const {v4: uuidv4 }= require('uuid');
-console.log(uuidv4());
+// const {v4: uuidv4 }= require('uuid');
+// console.log(uuidv4());
+const express= require('express');
+const app= express();
+const PORT=3000
+app.get('/',(req, res)=>{
+res.send('hello from node');
 
+})
+app.listen(PORT,()=>{
+    console.log('listening on 3000 ...')
+})
 
